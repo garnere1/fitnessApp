c96a4ba0f23cb62884178f3e002a5ed86e88a1c3
garnere1 <emilygarner24@gmail.com>
2023-07-11 19:56:55 -0400

    Inputscreen beginning

--- myProject/src/screens/Tabs/InputScreen/InputScreen.js
+++ myProject/src/screens/Tabs/InputScreen/InputScreen.js
@@ -1,14 +1,124 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import { StyleSheet, View, TextInput, Button, Keyboard, Alert } from 'react-native';
+import DropDownPicker from 'react-native-dropdown-picker';
+
 
 const InputScreen = () => {
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState(null);
+  const [items, setItems] = useState([
+    {label: 'Squat', value: 'squat'},
+    {label: 'Bench', value: 'bench'},
+    {label: 'Deadlift', value: 'deadlift'}
+  ]);
+
+  const [number, setNumber] = React.useState('');
+  const [reps, setReps] = React.useState('');
+
+  const onChanged = (text) => {
+      let newText = '';
+      let numbers = '0123456789';
+  
+      for (var i=0; i < text.length; i++) {
+          if(numbers.indexOf(text[i]) > -1 ) {
+              newText = newText + text[i];
+          }
+          else {
+              alert("please enter numbers only");
+          }
+      }
+      setNumber(newText);
+  }
+  const onChanged1 = (text) => {
+    let newText = '';
+    let numbers = '0123456789';
+
+    for (var i=0; i < text.length; i++) {
+        if(numbers.indexOf(text[i]) > -1 ) {
+            newText = newText + text[i];
+        }
+        else {
+            alert("please enter numbers only");
+        }
+    }
+    setReps(newText);
+}
+
+  const onPress = () => {
+      if (number.length !== 0) {
+          Alert.alert(
+              "Confirm Number",
+              number,
+              [
+                  {
+                      text: "Cancel",
+                      onPress: () => console.log("Cancel Pressed"),
+                  },
+
+                  { 
+                      text: "OK", 
+                      onPress: () => console.log("OK Pressed"),
+                  },
+              ],
+          );
+          setNumber('');
+          setReps('');
+          Keyboard.dismiss();
+      }
+  }
   return (
-    <View>
-      <Text>
-        Input
-      </Text>
+    <View style={styles.container}>
+      <DropDownPicker
+        open={open}
+        value={value}
+        items={items}
+        setOpen={setOpen}
+        setValue={setValue}
+        setItems={setItems}
+      />
+      <TextInput
+          keyboardType='numeric'
+          onChangeText={text => onChanged(text)}
+          value={number}
+          style={styles.input}
+          placeholder='Weight'
+          maxLength={10}
+      />
+      <TextInput
+          keyboardType='numeric'
+          onChangeText={text1 => onChanged1(text1)}
+          value={reps}
+          style={styles.input}
+          placeholder='Reps'
+          maxLength={10}
+      />
+      <View style={styles.buttonContainer}>
+          <Button 
+              title='Submit'
+              onPress={() => onPress()}
+          />
+      </View>
     </View>
+
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+      flex:1,
+      justifyContent:'center',
+      alignItems:'center',
+  },
+  input: {
+      borderWidth:1,
+      width:'80%',
+      borderColor:'#c7c3c3',
+      padding:10,
+  },
+  buttonContainer: {
+      marginBottom: 1,
+      width:'80%',
+  },
+})
 
 export default InputScreen;
